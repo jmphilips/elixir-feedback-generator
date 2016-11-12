97f2cb55db2e609e90b8d6e5d64b56500e7f08da
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-11 20:49:57 -0600

    separated the content_output into a new module

--- lib/speechfeedback.ex
+++ lib/speechfeedback.ex
@@ -1,30 +1,3 @@
 defmodule Speechfeedback do
 
-    
-
-    def content_output(grade) when grade > 7 do
-        content_good
-    end
-
-    def content_output(grade) when grade > 3 do
-        content_okay
-    end
-
-    def content_output(grade) when grade >= 0 do
-        content_bad
-    end
-
-
-    def content_good do
-        "Your Speech was very interesting."
-    end
-
-    def content_okay do
-        "You found some good information about your topic."
-    end
-
-    def content_bad do
-        "You did not try to find much information about your topic."
-    end
-
 end
